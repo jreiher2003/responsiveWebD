a22c4bdee6f36f26c755064466273e19fb562959
Jeff Reiher <jeffreiher@gmail.com>
2015-07-21 11:49:51 -0400

    starter commit for layout shifter design

--- lesson-4/layout_shifter/app/__init__.py
+++ lesson-4/layout_shifter/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
-app.config.from_pyfile('config.py')
+# app.config.from_pyfile('config.py')
 
-import app.views
+from app import views
